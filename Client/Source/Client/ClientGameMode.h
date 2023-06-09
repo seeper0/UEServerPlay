d7d7686d23a69b8e041dd44ce9c3d001cca6d83f
seeper0 <seeper@hanmail.net>
2023-06-10 02:19:46 +0900

    초기화

---
+++ Client/Source/Client/ClientGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ClientGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AClientGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AClientGameMode();
+};
+
+
+
